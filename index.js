0230680ab5af024c94934c14db7df017ee4b6ffa
JosephYounes <joseph.younes52@hotmail.com>
2025-06-10 16:28:26 -0400

    this is my menu lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+var burgers = ['Hamburger', 'Cheeseburger'];
+var featuredDrink = 'Strawberry Milkshake';
+function addBurger() {
+  let newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+if (true) {
+  let anothernewBurger = 'Maple Bacon Burger';
+  burgers.push(anothernewBurger);
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+}
